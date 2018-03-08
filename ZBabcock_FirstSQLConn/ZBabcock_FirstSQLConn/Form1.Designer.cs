e0fc2487308c9546e01f34a37cb5eccaad4a58d1
babbincocks <zabbybab@gmail.com>
2018-03-07 19:51:52 -0500

    Pretty sure I finished. Set up the address for the school computer in script for the stored procedure I created, and created the necessary objects to properly display the data the code retrieves.

---
+++ ZBabcock_FirstSQLConn/ZBabcock_FirstSQLConn/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ZBabcock_FirstSQLConn
+{
+    partial class frmVendor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridVend = new System.Windows.Forms.DataGridView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridVend)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridVend
+            // 
+            this.dataGridVend.AllowUserToOrderColumns = true;
+            this.dataGridVend.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.dataGridVend.Location = new System.Drawing.Point(12, 12);
+            this.dataGridVend.Name = "dataGridVend";
+            this.dataGridVend.ReadOnly = true;
+            this.dataGridVend.Size = new System.Drawing.Size(694, 341);
+            this.dataGridVend.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(580, 359);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(126, 28);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // frmVendor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 399);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dataGridVend);
+            this.Name = "frmVendor";
+            this.Text = "AW Vendor Data";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridVend)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridVend;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ ZBabcock_FirstSQLConn/ZBabcock_FirstSQLConn/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZBabcock_FirstSQLConn
+{
+    public partial class frmVendor : Form
+    {
+        public frmVendor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {         //Edit first row of connString to change the server, second row for database.
+            string connString = @"Server=PL1\MTCDB;
+                                    Database=AdventureWorks2012;
+                                        Trusted_Connection=True";
+
+            SqlConnection sqlConn = new SqlConnection(connString);
+
+            SqlDataAdapter sqlDataAd = new SqlDataAdapter("sp_VendorInfo", sqlConn);
+
+            DataTable dtVendors = new DataTable();
+
+            sqlDataAd.Fill(dtVendors);
+
+            dataGridVend.DataSource = dtVendors;
+        }
+    }
+}
